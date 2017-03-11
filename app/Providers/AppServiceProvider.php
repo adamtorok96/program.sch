22e047ccd5f0fffec208e63158cc4e404618f9d7
Adam Torok <adamtorok@sch.bme.hu>
2017-03-11 16:03:48 +0100

    Max db string length set

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,6 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(190);
+
         $socialite = $this->app->make('Laravel\Socialite\Contracts\Factory');
 
         $socialite->extend('sch', function ($app) use ($socialite) {
