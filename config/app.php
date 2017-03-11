c1e2010333227bfdda15eeb224ad584b4a49f2b5
Adam Torok <adamtorok@sch.bme.hu>
2017-03-11 12:41:39 +0100

    redirecting error log for heroku

--- config/app.php
+++ config/app.php
@@ -120,7 +120,7 @@ return [
     |
     */
 
-    'log' => env('APP_LOG', 'single'),
+    'log' => env('APP_LOG', 'errorlog'), // errorlog for heroku | default: single
 
     'log_level' => env('APP_LOG_LEVEL', 'debug'),
 
