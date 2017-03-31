a4d92d090a5937de9d4bd389a98eef1db0d09f6c
Adam Torok <adamtorok@sch.bme.hu>
2017-03-31 18:15:00 +0200

    assets downloader script

--- assets.sh
+++ assets.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-wget http://rickp.sch.bme.hu/klubszoba/mix-manifest.json -O public/mix-manifest.json
-wget http://rickp.sch.bme.hu/klubszoba/css/app.84984162f5e826593f33.css -O public/css/app.84984162f5e826593f33.css
-wget http://rickp.sch.bme.hu/klubszoba/js/app.6b029d2a67fab2747d5b.js -O public/js/app.6b029d2a67fab2747d5b.js
+wget http://rickp.sch.bme.hu/program/mix-manifest.json -O public/mix-manifest.json
+wget http://rickp.sch.bme.hu/program/css/app.84984162f5e826593f33.css -O public/css/app.84984162f5e826593f33.css
+wget http://rickp.sch.bme.hu/program/js/app.6b029d2a67fab2747d5b.js -O public/js/app.6b029d2a67fab2747d5b.js
