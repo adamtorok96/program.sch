3de5efa9326d7a34ad48c40634cb710d31049f37
Adam Torok <adamtorok@sch.bme.hu>
2017-04-12 18:59:49 +0200

    bug fixing...

--- app/Services/GoogleService.php
+++ app/Services/GoogleService.php
@@ -17,7 +17,7 @@ class GoogleService
     {
         putenv('GOOGLE_APPLICATION_CREDENTIALS=' . base_path('google.json'));
 
-
+/*
         dd(
             env('GOOGLE_APPLICATION_CREDENTIALS'),
             file_get_contents(env('GOOGLE_APPLICATION_CREDENTIALS')),
@@ -28,7 +28,7 @@ class GoogleService
                 file_get_contents(env('GOOGLE_APPLICATION_CREDENTIALS')), true
             )
         );
-
+*/
         $this->client = new Google_Client();
         $this->client->useApplicationDefaultCredentials();
         $this->client->addScope([Google_Service_Calendar::CALENDAR]);
