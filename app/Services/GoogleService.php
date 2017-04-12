ed7af2656ce2f99b8509298aea99efe0d0ce5481
Adam Torok <adamtorok@sch.bme.hu>
2017-04-12 18:45:07 +0200

    bug fixing...

--- app/Services/GoogleService.php
+++ app/Services/GoogleService.php
@@ -17,8 +17,8 @@ class GoogleService
     {
         putenv('GOOGLE_APPLICATION_CREDENTIALS=' . base_path('google.json'));
 
-        
-        dd(env('GOOGLE_APPLICATION_CREDENTIALS'), json_decode(env('GOOGLE_APPLICATION_CREDENTIALS'), true));
+
+        dd(env('GOOGLE_APPLICATION_CREDENTIALS'), json_decode(file_get_contents(env('GOOGLE_APPLICATION_CREDENTIALS'), true)));
 
         $this->client = new Google_Client();
         $this->client->useApplicationDefaultCredentials();
