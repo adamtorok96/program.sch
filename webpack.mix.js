f0e3834751e118e60de3b4217b3846c21ebfdb74
Adam Torok <adamtorok@sch.bme.hu>
2017-07-05 19:18:56 +0200

    npm update

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,23 @@
 const mix = require('laravel-mix');
-const gulp = require('gulp');
-const scp = require('gulp-scp2');
 
 mix
     .sass('resources/assets/sass/app.scss', 'public/css')
     .js('resources/assets/js/app.js', 'public/js')
-    .copy('node_modules/font-awesome/fonts', 'public/fonts')
     .version();
+
+const fonts = [
+    'node_modules/bootstrap-sass/assets/fonts/bootstrap',
+    'node_modules/font-awesome/fonts',
+    'node_modules/roboto-fontface/fonts/Roboto'
+];
+
+fonts.forEach(function (font) {
+    mix.copy(font, 'public/fonts')
+});
+
+const files = [
+    'public/css',
+    'public/js',
+    'public/fonts',
+    'public/mix-manifest.json'
+];
