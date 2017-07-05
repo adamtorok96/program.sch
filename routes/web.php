fbaf9f644fe0876d161b5e5eaeb5050985601ed2
Adam Torok <adamtorok@sch.bme.hu>
2017-07-05 17:09:44 +0200

    Route reorder

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::group(['prefix' => 'calendar', 'as' => 'calendar.'], function()
 
 Route::group(['prefix' => 'auth', 'as' => 'auth.'], function()
 {
-    Route::get('logout', 'AuthController@logout')->name('logout');
+    Route::get('logout', 'AuthController@logout')->name('logout')->middleware('auth');
     Route::get('{provider}/redirect', 'AuthController@redirect')->name('redirect');
     Route::get('{provider}/callback', 'AuthController@callback')->name('callback');
 });
@@ -43,92 +43,6 @@ Route::group(['middleware' => 'auth'], function()
 
 Route::get('programs/{program}', 'ProgramsController@show')->name('programs.show');
 
-Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'middleware' => ['auth', 'role:admin']], function ()
-{
-    Route::get('/', 'IndexController@index')->name('index');
-
-    Route::group(['prefix' => 'programs', 'as' => 'programs.'], function()
-    {
-        Route::get('/', 'ProgramsController@index')->name('index');
-        Route::get('create', 'ProgramsController@create')->name('create');
-        Route::post('store', 'ProgramsController@store')->name('store');
-        Route::get('edit/{program}', 'ProgramsController@edit')->name('edit');
-        Route::post('update/{program}', 'ProgramsController@update')->name('update');
-        Route::get('destroy/{program}', 'ProgramsController@destroy')->name('destroy');
-        Route::get('{program}', 'ProgramsController@show')->name('show');
-    });
-
-    Route::group(['prefix' => 'resorts', 'as' => 'resorts.'], function()
-    {
-        Route::get('/', 'ResortsController@index')->name('index');
-        Route::get('create', 'ResortsController@create')->name('create');
-        Route::post('store', 'ResortsController@store')->name('store');
-        Route::get('edit/{resort}', 'ResortsController@edit')->name('edit');
-        Route::post('update/{resort}', 'ResortsController@update')->name('update');
-        Route::get('{resort}', 'ResortsController@show')->name('show');
-    });
-
-    Route::group(['prefix' => 'circles', 'as' => 'circles.'], function()
-    {
-        Route::get('/', 'CirclesController@index')->name('index');
-        //Route::get('create', 'CirclesController@create')->name('create');
-        //Route::post('store', 'CirclesController@store')->name('store');
-        Route::get('edit/{circle}', 'CirclesController@edit')->name('edit');
-        Route::post('update/{circle}', 'CirclesController@update')->name('update');
-        Route::get('activate/{circle}', 'CirclesController@activate')->name('activate');
-        Route::get('deactivate/{circle}', 'CirclesController@deactivate')->name('deactivate');
-        Route::get('{circle}', 'CirclesController@show')->name('show');
-    });
-
-    Route::group(['prefix' => 'locations', 'as' => 'locations.'], function()
-    {
-        Route::get('/', 'LocationsController@index')->name('index');
-        Route::get('create', 'LocationsController@create')->name('create');
-        Route::post('store', 'LocationsController@store')->name('store');
-        Route::get('edit/{location}', 'LocationsController@edit')->name('edit');
-        Route::post('update/{location}', 'LocationsController@update')->name('update');
-        Route::post('destroy/{location}', 'LocationsController@destroy')->name('destroy');
-        Route::get('{location}', 'LocationsController@show')->name('show');
-    });
-
-    Route::group(['prefix' => 'posters', 'as' => 'posters.'], function()
-    {
-        Route::get('/', 'PostersController@index')->name('index');
-        Route::get('destroy/{poster}', 'PostersController@destroy')->name('destroy');
-    });
-
-    Route::group(['prefix' => 'users', 'as' => 'users.'], function()
-    {
-        Route::get('/', 'UsersController@index')->name('index');
-        Route::get('edit/{user}', 'UsersController@edit')->name('edit');
-        Route::post('update/{user}', 'UsersController@update')->name('update');
-        Route::get('toggle/pr/{user}/{circle}', 'UsersController@togglePr')->name('toggle.pr');
-
-        Route::group(['prefix' => 'promote', 'as' => 'promote.'], function()
-        {
-            Route::get('admin/{user}', 'UsersController@promoteAdmin')->name('admin');
-        });
-
-        Route::group(['prefix' => 'demote', 'as' => 'demote.'], function()
-        {
-            Route::get('admin/{user}', 'UsersController@demoteAdmin')->name('admin');
-        });
-
-        Route::get('{user}', 'UsersController@show')->name('show');
-    });
-
-    Route::group(['prefix' => 'ajax', 'as' => 'ajax.'], function()
-    {
-        Route::get('programs', 'AjaxController@programs')->name('programs');
-        Route::get('resorts', 'AjaxController@resorts')->name('resorts');
-        Route::get('circles', 'AjaxController@circles')->name('circles');
-        Route::get('circles/{circle}/users', 'AjaxController@circlesUsers')->name('circles.users');
-        Route::get('users', 'AjaxController@users')->name('users');
-        Route::get('locations', 'AjaxController@locations')->name('locations');
-        //Route::get('posters', 'AjaxController@posters')->name('posters');
-    });
-});
-
 if( config('app.debug') ) {
     Route::group(['prefix' => 'test', 'as' => 'test.'], function ()
     {
@@ -136,11 +50,3 @@ if( config('app.debug') ) {
         Route::get('admin', 'TestController@makeAdmin');
     });
 }
-
-/*
- * "php artisan clear-compiled",
-            "php artisan view:clear",
-            "php artisan cache:clear",
-            "composer dump-autoload",
-            "php artisan migrate:refresh --seed --env production"
- */
