4e26f729f8bc8d74f898fd3d6e40f57fff1eebb8
edems <adamtorok@sch.bme.hu>
2018-03-30 23:49:43 +0200

    Trix Editor added

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,9 +1,16 @@
 window.$ = window.jQuery = require('jquery');
 
+// Bootstrap
 require('bootstrap-sass');
 
+// Bootstrap Table
 require('bootstrap-table');
 
+// JQuery DateTimePicker
 require('jquery-datetimepicker');
 
+// Trix editor
+require('trix');
+
+// JQuery UI AutoComplete
 import 'jquery-ui/ui/widgets/autocomplete.js';
