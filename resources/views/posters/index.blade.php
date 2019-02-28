bc89e9bcd477a1a7ec9e888cb6d7768dbd3ade7b
edems <adamtorok@sch.bme.hu>
2019-02-28 17:29:45 +0100

    posters listing fix

--- resources/views/posters/index.blade.php
+++ resources/views/posters/index.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.layout')
 
-@section('title', 'Poszterek')
+@section('title', 'Plakátok')
 @section('icon', 'images')
 
 @section('content')
+    @include('layouts.title-center')
+
     <div class="row">
         @each('posters.poster', $posters, 'poster')
     </div>
