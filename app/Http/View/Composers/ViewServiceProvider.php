51e855995c0252af58e8c37c28a8972a652392fb
edems <adamtorok@sch.bme.hu>
2019-03-24 13:39:16 +0100

    ViewServiceProvider and view composer added for navbar support

---
+++ app/Http/View/Composers/ViewServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Http\View\Composers;
+
+
+use App\Models\Circle;
+use Auth;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class ViewServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        View::composer('layouts.navbar', function (\Illuminate\View\View $view) {
+            if( Auth::check() ) {
+                $circles = Circle::WherePRManager(Auth::user())->get();
+
+                $view->with('circles', $circles);
+            }
+        });
+    }
+
+}
