ebb858cb618ba8495b1980bddd06dd30db749449
edems <adamtorok@sch.bme.hu>
2019-03-24 13:42:02 +0100

    email layout added

---
+++ resources/views/mails/newsletter-mails/layout.blade.php
@@ -0,0 +1,7 @@
+@markdown($msg)
+<br>
+<hr>
+<br>
+Ez az üzenet a <a href="{{ config('app.url') }}"><b>Program.sch</b></a> oldalról érkezett a <b>{{ $circle->name }}</b> körtől. Kérlek a levélre ne válaszolj.<br>
+Amennyiben nem szeretnél több, ehhez hasonló üzenetet kapni, úgy le tudsz róla iratkozni az oldalon.<br>
+További szép napot!
