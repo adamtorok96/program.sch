681942d63975f135c27db929aad5bfd1b438d844
edems <adamtorok@sch.bme.hu>
2019-03-24 13:39:56 +0100

    NewsletterEmail Mailable added

---
+++ app/Mail/NewsletterEmail.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\NewsletterMail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class NewsletterEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * @var NewsletterMail
+     */
+    private $newsletterMail;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param NewsletterMail $newsletterMail
+     */
+    public function __construct(NewsletterMail $newsletterMail)
+    {
+        $this->newsletterMail = $newsletterMail;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this
+            ->to(config('mail.from.address'))
+            ->bcc($this->newsletterMail->getParticipants())
+            ->subject($this->newsletterMail->subject)
+            ->view('mails.newsletter-mails.layout', [
+                'msg'       => $this->newsletterMail->message,
+                'circle'    => $this->newsletterMail->circle
+            ])
+        ;
+    }
+}
