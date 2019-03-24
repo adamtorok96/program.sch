25451a98ad68a850df899078c72962724f9542af
edems <adamtorok@sch.bme.hu>
2019-03-24 13:41:20 +0100

    js and css upgrade for markdown support

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,9 +4,13 @@ require('bootstrap-sass');
 
 require('bootstrap-table');
 
+require('bootstrap-markdown-editor');
+
 require('jquery-datetimepicker');
 
 import 'jquery-ui/ui/widgets/autocomplete.js';
+
+window.Showdown = require('showdown');
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
