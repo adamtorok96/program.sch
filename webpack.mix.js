06d39bd91bf02678dcecfff4448b5722a2360349
edems <adamtorok@sch.bme.hu>
2019-03-27 15:57:07 +0100

    google analytic code separated

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,14 @@
 const mix = require('laravel-mix');
 
-mix
-    .sass('resources/assets/sass/app.scss', 'public/css')
-    .js('resources/assets/js/app.js', 'public/js')
-    .version()
-;
+const styles = [
+    'resources/assets/sass/app.scss'
+];
+
+const scripts = [
+    'resources/assets/js/app.js',
+    'resources/assets/js/google-analytic.js',
+    'resources/assets/js/admin.js'
+];
 
 const fonts = [
     'node_modules/bootstrap-sass/assets/fonts/bootstrap',
@@ -13,6 +17,16 @@ const fonts = [
     'node_modules/raleway-webfont/fonts'
 ];
 
+styles.forEach(function (style) {
+   mix.sass(style, 'public/css');
+});
+
+scripts.forEach(function (script) {
+    mix.js(script, 'public/js');
+});
+
 fonts.forEach(function (font) {
     mix.copy(font, 'public/fonts')
 });
+
+mix.version();
